cd900e57063fb4f6934c05eda59ec06586b7bb35
Margarita <alis-weasley@mail.ru>
2022-11-22 19:56:55 +0300

    fix: исправить опечатки

--- 09-is-prime/script.js
+++ 09-is-prime/script.js
@@ -11,9 +11,7 @@
 
 function isPrime(n) {
   for (let i = 2; i < n; i++) {
-    if (n % i === 0) {
-      return false;
-    }
+    if (n % i === 0) return false;
   }
   if (n <= 1) return false;
   else return true;
